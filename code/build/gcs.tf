0a1980ab11d3bf610527f05c6ff1203ac8081c29
david17pix <dantolinez@paloaltonetworks.com>
2026-01-27 16:53:26 -0500

    Update gcs.tf

--- code/build/gcs.tf
+++ code/build/gcs.tf
@@ -1,1 +1,29 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+provider "google" {
+  project = "{{{project_0.project_id}}}"
+  region  = "us-central1"
+}
+
+resource "random_id" "rand_suffix" {
+  byte_length = 4
+}
+
+resource "google_storage_bucket" "example" {
+  name     = "demo-${random_id.rand_suffix.hex}"
+  location = "us-central1"
+  force_destroy = true
+
+  uniform_bucket_level_access = false
+}
+
+output "bucket_name" {
+  value = google_storage_bucket.example.name
+}
 
